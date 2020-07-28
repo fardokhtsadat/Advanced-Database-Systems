7607ae6cb29864d972b089255fc8f82b92093ac7
Fardokhtsadat Mohammadi <fardokht.fm@gmail.com>
2020-07-28 14:19:25 +0200

    Update importANDmodify.sql

--- importANDmodify.sql
+++ importANDmodify.sql
@@ -3,148 +3,86 @@
 CREATE DATABASE moviesDB;
 
 -------------------------------------------
------reate tables for the big dataset------
+-----create and load tables for the big dataset------
 -------------------------------------------
 
-#### table: titles ####
+
+It contains 27753444 ratings and 1108997 tag applications across 58098 movies. These data were created by 283228 users between January 09, 1995 and September 26, 2018.
+
+######################
+#### table: users ####
+######################
+CREATE TABLE users(
+    userId INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (userId)
+);
+
+LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/userId.csv' INTO TABLE users
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(userId);
+
+mysql> Query OK, 283228 rows affected (1.19 sec)
+mysql> Records: 283228  Deleted: 0  Skipped: 0  Warnings: 0
+
+######################
+#### table: movies ####
+######################
 # the original table "movies" is splited into two tables of "titles" and "genres"
 CREATE TABLE titles(
-    movieId int(10),
-    title varchar(300) NOT NULL
+    movieId INT NOT NULL AUTO_INCREMENT,
+    title varchar(300) NOT NULL,
+    PRIMARY KEY (movieId)
     );
 
-ALTER TABLE titles ADD CONSTRAINT movieId_pk PRIMARY KEY (movieId);
-
-##ALTER TABLE genres MODIFY genres varchar(30) NOT NULL;
-
 CREATE TABLE genres(
-    movieId INT(10),
+    id INT NOT NULL AUTO_INCREMENT,
+    movieId INT NOT NULL,
     genres VARCHAR(30) NOT NULL,
-    FOREIGN KEY (movieId) REFERENCES movieId_title (movieId)
-)
-
-##ALTER TABLE movieId_genres ADD FOREIGN KEY (movieId) REFERENCES movieId_title (movieId);
-##ALTER TABLE movieId_genres MODIFY movieId INT(10) NOT NULL;
-
-
-#### table: links ####
-CREATE TABLE links(
-    movieId NUMBER(5) CONSTRAINT links_movieId_fk REFERENCES movieId_title(movieId),
-    imdbId NUMBER(20) CONSTRAINT imdbId_uk UNIQUE,
-    tmdbId NUMBER(20) CONSTRAINT imdbId_uk UNIQUE
-)
-
-CREATE TABLE links(
-    movieId INT NOT NULL,
-    imdbId INT,
-    tmdbId INT
+    PRIMARY KEY (id)
 );
-ALTER TABLE links ADD FOREIGN KEY (movieId) REFERENCES movieId_title (movieId);
+ALTER TABLE genres ADD FOREIGN KEY (movieId) REFERENCES titles (movieId);
 
-UPDATE links 
-SET tmdbId = NULL 
-WHERE tmdbId  = 0;
-
-
-#### table: users ####
-CREATE TABLE users(
-    userId INT(10)
-);
-ALTER TABLE users ADD CONSTRAINT userId_pk PRIMARY KEY (userId);
-
-#### table: tags ####
-CREATE TABLE tags(
-    userId INT(10), 
-    movieId INT,
-    tag varchar(60) NOT NULL,
-    timestamp INT(11) NOT NULL
-);
-ALTER TABLE tags ADD FOREIGN KEY (movieId) REFERENCES movieId_title (movieId);
-ALTER TABLE tags ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE tags MODIFY movieId INT NOT NULL;
-ALTER TABLE tags MODIFY userId INT NOT NULL;
-
-CONSTRAINT tags_userId_fk REFERENCES users(userId),
-CONSTRAINT tag_nn NOT NULL,
-    
-
-#### table: ratings ####
-CREATE TABLE ratings(
-    userId INT,
-    movieId INT, 
-    rating DECIMAL(2,1) NOT NULL,
-    timestamp INT NOT NULL
-);
-ALTER TABLE ratings ADD FOREIGN KEY (movieId) REFERENCES movieId_title (movieId);
-ALTER TABLE ratings ADD FOREIGN KEY (userId) REFERENCES users (userId);
-ALTER TABLE ratings MODIFY userId INT NOT NULL;
-ALTER TABLE ratings MODIFY movieId INT NOT NULL;
-
-
-#### table: genome_tags ####
-CREATE TABLE genome_tags(
-    tagId INT NOT NULL,
-    tag varchar(100) NOT NULL
-);
-ALTER TABLE genome_tags ADD CONSTRAINT genome_tags_tagId_pk PRIMARY KEY (tagId);
-
-CREATE TABLE genome_scores(
-    movieId INT NOT NULL,
-    tagId INT NOT NULL, 
-    relevance DECIMAL(6,5) NOT NULL
-);
-ALTER TABLE genome_scores ADD FOREIGN KEY (movieId) REFERENCES movieId_title (movieId);
-ALTER TABLE genome_scores ADD FOREIGN KEY (tagId) REFERENCES genome_tags (tagId);
-
-
-
-
-------loading data frov csv to DB------
-LOAD DATA INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/movieId_title.csv' 
-INTO TABLE movieId_title 
+LOAD DATA INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/movieId_title.csv' 
+INTO TABLE titles 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (movieId, title);
 
-LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/ml-latest-small/links.csv' INTO TABLE links
+mysql> 58098 records imported
+
+LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/ml-latest-small/movieId_genres.csv' INTO TABLE genres
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(movieId, imdbId, tmdbId);
+(movieId, genres);
 
+mysql> 106107 records imported
 
-LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/tags.csv' INTO TABLE tags
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(userId, movieId, tag, timestamp);
+######################
+#### table: links ####
+######################
 
+CREATE TABLE links(
+    id INT NOT NULL AUTO_INCREMENT,
+    movieId INT NOT NULL,
+    imdbId INT,
+    tmdbId INT,
+    PRIMARY KEY (id)
+);
+ALTER TABLE links ADD FOREIGN KEY (movieId) REFERENCES titles (movieId);
 
-LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/ratings.csv' INTO TABLE ratings
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(userId, movieId, rating, timestamp);
+set foreign_key_checks = 0;
 
+UPDATE links 
+SET tmdbId = NULL 
+WHERE tmdbId  = 0;
 
-LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/genome-tags.csv' INTO TABLE genome_tags
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(tagId, tag);
-
-LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/genome-scores.csv' INTO TABLE genome_scores
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(movieId, tagId, relevance);
+set foreign_key_checks = 1;
 
 LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/links.csv' INTO TABLE links
 FIELDS TERMINATED BY ',' 
@@ -153,16 +91,104 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (movieId, imdbId, tmdbId);
 
-LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/userId.csv' INTO TABLE users
+mysql> Query OK, 58098 rows affected, 181 warnings (0.46 sec)
+mysql> Query OK, 58098 rows affected, 181 warnings (0.46 sec)
+
+#######################
+#### table: ratings ###
+#######################
+
+CREATE TABLE ratings(
+    id INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL,
+    movieId INT NOT NULL, 
+    rating DECIMAL(2,1) NOT NULL,
+    timestamp INT NOT NULL,
+    PRIMARY KEY (id)
+);
+ALTER TABLE ratings ADD FOREIGN KEY (movieId) REFERENCES titles (movieId);
+ALTER TABLE ratings ADD FOREIGN KEY (userId) REFERENCES users (userId);
+ALTER TABLE ratings ADD CONSTRAINT user_movie_uc UNIQUE (userId,movieId);
+
+LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/ratings.csv' INTO TABLE ratings
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(userId);
+(userId, movieId, rating, timestamp);
+
+mysql> Query OK, 27753444 rows affected (8 min 10.36 sec)
+mysql> Records: 27753444  Deleted: 0  Skipped: 0  Warnings: 0
+
+####################
+#### table: tags ###
+####################
+CREATE TABLE tags(
+    id INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL, 
+    movieId INT NOT NULL,
+    tag varchar(60) NOT NULL,
+    timestamp INT NOT NULL,
+    PRIMARY KEY (id)
+);
+ALTER TABLE tags ADD FOREIGN KEY (movieId) REFERENCES titles (movieId);
+ALTER TABLE tags ADD FOREIGN KEY (userId) REFERENCES users (userId);
+
+LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/tags.csv' INTO TABLE tags
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(userId, movieId, tag, timestamp);
+
+mysql> Query OK, 1108997 rows affected, 570 warnings (15.96 sec)
+mysql> Records: 1108997  Deleted: 0  Skipped: 0  Warnings: 570
+
+###########################
+#### table: genome_tags ###
+###########################
+
+CREATE TABLE genome_tags(
+    tagId INT NOT NULL AUTO_INCREMENT,
+    tag varchar(100) NOT NULL,
+    PRIMARY KEY (tagId)
+);
+
+LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/genome-tags.csv' INTO TABLE genome_tags
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(tagId, tag);
+
+mysql> Query OK, 1128 rows affected (0.01 sec)
+mysql> Records: 1128  Deleted: 0  Skipped: 0  Warnings: 0
+
+###########################
+#### table: genome_scores ###
+###########################
+CREATE TABLE genome_scores(
+    id INT NOT NULL AUTO_INCREMENT,
+    movieId INT NOT NULL,
+    tagId INT NOT NULL, 
+    relevance DECIMAL(6,5) NOT NULL,
+    PRIMARY KEY (id)
+);
+ALTER TABLE genome_scores ADD FOREIGN KEY (movieId) REFERENCES titles (movieId);
+ALTER TABLE genome_scores ADD FOREIGN KEY (tagId) REFERENCES genome_tags (tagId);
 
 
-ALTER TABLE ratings ADD FOREIGN KEY (movieId) REFERENCES movieId_title (movieId);
-ALTER TABLE ratings MODIFY movieId INT NOT NULL;
+LOAD DATA LOCAL INFILE '/Users/fardokht/Desktop/JCU/second-semester/database_systems/final_project/ml-latest/genome-scores.csv' INTO TABLE genome_scores
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(movieId, tagId, relevance);
 
+mysql> Query OK, 14862528 rows affected, 65535 warnings (3 min 23.70 sec)
+mysql> Records: 14862528  Deleted: 0  Skipped: 0  Warnings: 11399920
+
+warnings:
+ Note  | 1265 | Data truncated for column 'relevance' at row 1426
 
 
